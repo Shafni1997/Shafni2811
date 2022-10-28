5939d6cd8eb31094aa28e23c4a4e63813cebb1d6
Shafniamdt <iamshafnirafaideen@gmail.com>
2022-10-28 18:17:25 +0530

    Pushed arrays and objects

---
+++ Js/array.js
@@ -0,0 +1,16 @@
+var cars = ["Merc", "Jaguar", "Nissan", "Lambo"];
+console.log(cars);
+cars.push("Ferrari");
+console.log(cars);
+cars.pop();
+cars.pop();
+console.log(cars);
+
+
+var books = {
+    author: "Shafni",
+    price: 100
+};
+
+console.log(books.author);
+
---
+++ Js/practicalobjects.js
@@ -0,0 +1,14 @@
+var cars = ["Car1", "Car2", "Car3"];
+console.log(cars);
+cars = [];
+cars.push("Shafni");
+console.log(cars);
+
+
+
+var books = {
+    author: "Jimmy",
+    price: 100
+};
+
+console.log(books);
